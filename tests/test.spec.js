a9b2031d7c96cef32698d50c33f380d32b55ad9e
oliverrochester <ogrochester@gmail.com>
2024-12-04 22:26:38 -0500

    working - updated wording

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -6,22 +6,36 @@ const credentials = require('../credentials.json')
 const tests = testCases.testCases
 const { test, expect } = require('@playwright/test');
 
+//loop through all test cases from the test.json file
 for (let i = 0; i < tests.length; i++){
+
   test(`Test Case ${i + 1}`, async ({ page }) => {
+    //navigate to site and login
     await page.goto('https://animated-gingersnap-8cf7f2.netlify.app/');
     await page.locator('#username').fill(credentials.username);
     await page.locator('#password').fill(credentials.password);
     await page.getByRole('button', { name: 'Sign in' }).click();
+
+    //navigate to necessary page of the site
     await page.getByRole('button', { name : tests[i].page }).click()
+
+    //get the container element of the correct column on the board and assert presence (To Do, In Progress, etc..)
     const columnContainer = page.getByRole('heading', { name : tests[i].column }).locator('..') //retrieving the column container (To Do, In Progress, etc...)
     await expect(columnContainer).toBeVisible({timeout:10000})
-    const itemTitle = columnContainer.getByRole('heading', { name: tests[i].itemTitle})
-    await expect(itemTitle).toBeVisible({timeout:10000});
-    const itemContainer = itemTitle.locator('..')
+
+    //get the title of the ticket within the associated column and assert presence
+    const ticketTitle = columnContainer.getByRole('heading', { name: tests[i].ticketTitle})
+    await expect(ticketTitle).toBeVisible({timeout:10000});
+
+    //get parent container element of ticket title for searching it's sub-elements
+    const ticketContainer = ticketTitle.locator('..')
+
+    //loops through all tags and asserts text match within the associated ticket
     for(let x = 0; x < tests[i].tags.length; x++){
-      const tag = itemContainer.locator('span').nth(x)
+      const tag = ticketContainer.locator('span').nth(x)
       await expect(tag).toHaveText(tests[i].tags[x])
     }
+
   });
 }
 
